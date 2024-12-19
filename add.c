06edad03c6c5bdab9e407c87d47ea98c5a950cbf
Veer212004 <153846364+Veer212004@users.noreply.github.com>
2024-12-19 12:16:42 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-  int num1,num2,sum;
-printf("Enter any two numbers: " );
-scanf("%d%d",&num1,&num2);
-sum= num1+num2;
-printf("The sum of %d and %d is %d",num1,num2 ,sum);
+  int num1,num2 num3,sum;
+printf("Enter any three numbers: " );
+scanf("%d%d%d",&num1,&num2,&num3);
+sum= num1+num2+num3;
+printf("The sum of %d , %d and %d is %d",num1,num2 , num3,sum);
 return 0;
 }
